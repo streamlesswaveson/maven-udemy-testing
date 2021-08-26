8a31237a061acb0cd1954237592bcf7a8afe4482
streamless <streamlesswaveson@gmail.com>
2021-08-26 06:35:12 -0700

    maven failsafe (integration test) plugin demo

---
+++ src/test/java/me/delete/JavaHelloWorldIT.java
@@ -0,0 +1,15 @@
+package me.delete;
+
+import org.junit.jupiter.api.Test;
+
+/*
+mvn verify
+ */
+public class JavaHelloWorldIT {
+
+
+    @Test
+    void helloIntegrationTest() {
+        System.out.println("my integration test");
+    }
+}
