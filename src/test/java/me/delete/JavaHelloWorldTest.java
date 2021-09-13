0c8ec8e87e606068396fea7a6a0840e735a8cd90
streamless <streamlesswaveson@gmail.com>
2021-09-13 05:32:56 -0700

    using build profiles to inject build properties

---
+++ src/test/java/me/delete/JavaHelloWorldTest.java
@@ -0,0 +1,14 @@
+package me.delete;
+
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    void hello() {
+        System.out.println("#########");
+        System.out.println(System.getenv("TEST_HOST"));
+    }
+
+}
